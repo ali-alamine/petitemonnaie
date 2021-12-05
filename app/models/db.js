7649d24c6791b4b62393d89de9679978172a42fe
Ali Al Amine <ali@melrok.com>
2021-12-05 22:30:39 +0200

    changed repo

---
+++ app/models/db.js
@@ -0,0 +1,20 @@
+'user strict';
+
+var mysql = require('mysql');
+//local mysql db connection
+
+var connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'petitem',
+      password : 'U<U[{Bc+C!3sdRn2{{2t}?#m',
+      database : 'sicomex_stores_new'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+module.exports = connection;
